1ead3cbd5fa789fe0ad31e38078c56342bddbece
winfan <121552591@qq.com>
2019-06-13 14:33:13 +0800

    update

---
+++ cloud/common.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+module.exports = {
+	"masterKey": {
+		useMasterKey: true
+	}
+}
--- cloud/main.js
+++ cloud/main.js
@@ -5,13 +5,28 @@ Parse.Cloud.define('hello', function(req, res) {
 	}
 });
 
+
 Parse.Cloud.define("averageStars", async (request) => {
-  const query = new Parse.Query("Review");
-  query.equalTo("movie", request.params.movie);
-  const results = await query.find();
-  let sum = 0;
-  for (let i = 0; i < results.length; ++i) {
-    sum += results[i].get("stars");
-  }
-  return sum / results.length;
+   let userId = req.params.id
+		// sessionToken = req.user.get("sessionToken");
+	// if(!userId || !sessionToken) return {
+	// 	"message": "参数不齐"
+	// }
+
+	let query = new Parse.Query(Parse.User);
+	// query.equalTo("user", {
+	// 	"__type": "Pointer",
+	// 	"className": "_User",
+	// 	"objectId": userId
+	// })
+	query.equalTo("objectId", userId);
+	query.limit(1);
+	try {
+		var objs = await query.find({useMasterKey: true});
+		objs.set('name',req.params.name)
+		await objs.save({useMasterKey: true})
+	} catch(e) {
+		return 'error'
+	}
+	return 1
 });
