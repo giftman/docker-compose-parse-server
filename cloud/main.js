a332381638ca44e6cfdf3e627038be7ddf94727a
winfan <121552591@qq.com>
2019-06-13 16:37:11 +0800

    update

--- cloud/main.js
+++ cloud/main.js
@@ -8,21 +8,18 @@ Parse.Cloud.define('hello', function(req, res) {
 
 Parse.Cloud.define("updateUser", async (req,res) => {
    let userId = req.params.id
-		// sessionToken = req.user.get("sessionToken");
+
+	// sessionToken = req.user.get("sessionToken");
 	// if(!userId || !sessionToken) return {
 	// 	"message": "参数不齐"
 	// }
 
 	let query = new Parse.Query(Parse.User);
-	// query.equalTo("user", {
-	// 	"__type": "Pointer",
-	// 	"className": "_User",
-	// 	"objectId": userId
-	// })
 	query.equalTo("objectId", userId);
 	query.limit(1);
 	try {
 		var objs = await query.find({useMasterKey: true});
+		//Todo  管理员及创建者才可以继续修改 否则返回非法操作
 		await objs[0].save({...req.params},{useMasterKey:true})
 	} catch(e) {
 		return e.message
