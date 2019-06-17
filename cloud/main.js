15486557a1d4ac67f96823c97309dc2b31046256
winfan <121552591@qq.com>
2019-06-17 18:36:52 +0800

    update

--- cloud/main.js
+++ cloud/main.js
@@ -73,7 +73,7 @@ Parse.Cloud.afterSave("Record", async (req) => {
 
 		console.log('before Save')
 		//save report 
-		const user = request.user
+		const user = req.user
 		console.log(user)
 
 		//先这样存 决断下月的有没有，没有就新建一份
