b730a7d654099b29a5368b9626c991991a38f438
winfan <121552591@qq.com>
2020-01-20 12:08:49 +0800

    work night

--- cloud/main.js
+++ cloud/main.js
@@ -71,7 +71,7 @@ Parse.Cloud.define("getMyUser", async (req,res) => {
 	let query = new Parse.Query(Parse.User);
 	query.equalTo("parents", userId);
 	query.descending("createdAt");
-	query.select("name","idcard","sex","age","phone","education","address","person_detail","des","target_job","night_job","target_salary","percentage","job","workers","jobName");
+	query.select("name","idcard","atten_uid","sex","age","phone","education","address","person_detail","des","target_job","night_job","target_salary","percentage","job","workers","jobName");
 	query.limit(300);
 	try {
 		var objs = await query.find({useMasterKey: true});
